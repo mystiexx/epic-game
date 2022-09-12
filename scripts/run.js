c299ffa3c1ba0f21c12ded94fde78d469a7d0b37
Roland Enola <aloneroland@gmail.com>
2022-09-12 09:05:05 +0100

    new

---
+++ scripts/run.js
@@ -0,0 +1,34 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["Leo", "Aang", "Pikachu"],
+    [
+      "https://i.imgur.com/pKd5Sdk.png", // Images
+      "https://i.imgur.com/xVu4vFL.png",
+      "https://i.imgur.com/WMB6g9u.png",
+    ],
+    [100, 200, 300], // HP values
+    [100, 50, 25],
+    "Elon Musk", // Boss name
+    "https://i.imgur.com/AksR0tt.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damageF
+  );
+  await gameContract.deployed();
+
+  console.log("contract deployed to:", gameContract.address);
+
+  
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
